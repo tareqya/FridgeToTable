a4c408e764b8354a73a58e91114d100e82994a9f
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-08 16:29:04 +0200

    first commit

---
+++ app/src/main/java/com/example/fridgetotable/auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.fridgetotable.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fridgetotable.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/fridgetotable/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.fridgetotable.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fridgetotable.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
